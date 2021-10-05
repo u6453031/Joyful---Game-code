985ebffaed7dec384c50b915750381fd1102d986
u6453031 <u6453031@anu.edu.au>
2021-10-05 19:43:29 +0800

    add cloth, hats, shoes

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div style="background-color: #E2F2F4; height: 100vh; width: 100vw;">
+  <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
