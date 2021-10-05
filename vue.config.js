17675f8d8f4f23dd9cb037cfbe0da8672db88a5c
u6453031 <56214526+u6453031@users.noreply.github.com>
2021-10-05 15:33:33 +0800

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // publicPath:process.env.NODE_ENV === 'production' ? '/vue_workspac/aihuhuproject/' : '/',
-
-    //基本路径
-    publicPath: './',//默认的'/'是绝对路径，如果不确定在根路径，改成相对路径'./'
-
-
-};
